1fe510ffeb15c63d5c6b2413be3cdbed579ed50a
maxmijatovic <maxima.mijatovic@gmail.com>
2019-12-02 18:19:16 +0100

    Update zavrsnijp21.sql

--- zavrsnijp21.sql
+++ zavrsnijp21.sql
@@ -53,3 +53,41 @@ alter table bird_count add foreign key (participant) references participant(id);
 
 alter table species_bird_count add foreign key (species) references species(id);
 alter table species_bird_count add foreign key (bird_count) references bird_count(id);
+
+insert into participant(id,name,surname,email) values
+(null,'Tibor','Mikuska','tmikuska@gmail.com'),
+(null,'Adrian','Tomik','atomik@gmail.com'),
+(null,'Josip','Ledinšćak','jledinscak@gmail.com'),
+(null,'Maksima','Mijatović','mmijatovic@gmail.com');
+
+insert into site(id,site_name,site_code,region,latitude,longitude) values
+(null,'Donji Miholjac fishponds','HR00225','Danube watershed',45.767071,18.198162),
+(null,'Topoljski Dunavac oxbow','HR00718','Danube watershed',45.862307,18.198162),
+(null,'Suza village - fields','HR10000','Danube watershed',45.774041,18.782845),
+(null,'Nasicka Breznica fishponds','HR00237','Danube watershed',45.56929,18.172459),
+(null,'Nijemci village','HR10000','Sava watershed',45.140546,19.034328),
+(null,'Danube river-6 (1341-1350 rkm Borovo)','HR00259','Danube watershed',45.426271,19.020424);
+
+insert into bird_count(id,date,coverage,quality,method,water,ice,tidal,weather,disturbed,site,participant) values
+(null,'2017-01-15','Excellent=76-100%','real count','Survey on foot or vehicle,Telescope used','Normal(wet)','Partly frozen < 90%','No tides','No effect','No effect',1,1),
+(null,'2019-01-29','Excellent=76-100%','real count','Survey on foot or vehicle,Telescope used','Normal(wet)','Complete frozen ','No tides','Little effect','No effect',2,2),
+(null,'2016-01-14','Bad=<25%','real count','Survey on foot or vehicle,Telescope used','Normal(wet)','Not frozen','No tides','No effect','No effect',3,4),
+(null,'2016-01-28','Excellent=76-100%','real count','Survey on foot or vehicle,Telescope used','Normal(wet)','Partly frozen < 90%','No tides','No effect','No effect',4,3),
+(null,'2015-01-26','Unknown','real count','Survey on foot or vehicle','Normal(wet)','Not frozen','No tides','No effect','No effect',5,4),
+(null,'2015-01-28','Bad=<25%','real count','Survey on foot or vehicle,Telescope used','Normal(wet)','Not frozen','No tides','No effect','No effect',6,2);
+
+insert into species(id,scientific_name_genus,scientific_name_species,english_name,species_group,species_code) values
+(null,'Ardea','cinerea','Grey Heron','Herons','ARDCI'),
+(null,'Buteo','buteo','Eurasian Buzzard','Raptors','BUTBU'),
+(null,'Falco','tinnunculus','Common Kestrel','Raptors','FALTI'),
+(null,'Alcedo','atthis','Common Kingfisher','Miscellaneous','ALCAT'),
+(null,'Asio','otus','Northern Long-eared Owl','Raptors','ASIOT'),
+(null,'Fulica','atra','Common Coot','Rails and Coots','FULAT');
+
+insert species_bird_count(species,bird_count,number_of_bird) values
+(1,1,270),
+(2,2,4),
+(3,3,2),
+(4,4,6),
+(5,5,38),
+(6,6,10);
